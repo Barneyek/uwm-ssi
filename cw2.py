eb92172607e0e4c9a6d14a20b8f07fd0e0ceb4d0
Damian Osiecki <osieckidamian1992@gmail.com>
2020-01-02 21:34:06 +0100

    Obliczenie listy parametrow systemu treningowego

--- cw2.py
+++ cw2.py
@@ -135,6 +135,59 @@ def getIndexOfDecision(array):
         result.append(decisionObject)
     return result
 
+def countParam(array, indexOfDecisions, trnArray):
+    i = 0
+    listOfDecisions = []
+    for checkOtherDecisions in indexOfDecisions:
+        listOfDecisions.append(checkOtherDecisions.getDecision())
+    favorizationList = []
+    listOfParam = []
+    for xX in array:
+        i += 1
+        param = Param()
+        for decision in indexOfDecisions:
+            param = Param()
+            param.setTestObject("x" + str(i))
+            param.setCObjet(decision.getDecision())
+            j = 0
+            listOfParamCounter = []
+            whichElem = 0
+            for elemOfX in xX:
+                if whichElem == len(xX) - 1:
+                    continue
+                counter = 0
+                for k in decision.getIndexList():
+                    if elemOfX == trnArray[k][whichElem]:
+                        counter += 1
+                if (counter == 0):
+                    checkIfAllZero = True
+                    for k in range(len(trnArray) - 1):
+                        if elemOfX == trnArray[k][whichElem]:
+                            checkIfAllZero = False
+                            break
+
+                    if checkIfAllZero == False:
+                        favorization = Favorization()
+                        favorization.setDecision(decision.getDecision())
+                        favorization.setWhichElem(whichElem)
+                        favorization.setTestObject(param.getTestObject())
+                        favorizationList.append(favorization)
+                whichElem += 1
+                listOfParamCounter.append(counter / len(decision.getIndexList()))
+            paramResult = (1 / 2) * sum(listOfParamCounter)
+            param.setParam(paramResult)
+            listOfParam.append(param)
+
+    for elem in listOfParam:
+        for favorization in favorizationList:
+            if elem.getTestObject() == favorization.getTestObject() and elem.getCObject() != favorization.getDecision():
+                length = 0
+                for decisionElem in indexOfDecisions:
+                    if decisionElem.getDecision() == elem.getCObject():
+                        length = len(decisionElem.getIndexList())
+                elem.setParam(elem.getParam() + ((1 / 2) * (1 / length)))
+    return listOfParam
+
 class NaiwnyKlasyfikatorBayesa():
     def main(self):
         fDec = open("result/dec_bayes.txt", "w+")
@@ -143,6 +196,7 @@ class NaiwnyKlasyfikatorBayesa():
         lines = delLastColumnAndRow(lines)
         australianTRN = delLastColumnAndRow(australianTRN)
         getTrnDecisions = getIndexOfDecision(australianTRN)
+        countedParams = countParam(lines, getTrnDecisions, australianTRN)
 
 if __name__ == "__main__":
     NaiwnyKlasyfikatorBayesa.main("args")
